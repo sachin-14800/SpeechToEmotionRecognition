001a7239e70eb75a9344dc76b952dbdb748e2d1e
Sachin Gupta <sachin.14800@gmail.com>
2021-03-21 15:17:57 +0530

    Basic functionality is ready

---
+++ test.py
@@ -0,0 +1,74 @@
+import librosa
+import soundfile
+import os, glob
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+def extract_feature(file_name,mfcc,chroma,mel):
+  with soundfile.SoundFile(file_name) as sound_file:
+    X=sound_file.read(dtype="float32")
+    sample_rate=sound_file.samplerate
+    if chroma:
+      stft=np.abs(librosa.stft(X))
+    result=np.array([])
+    if mfcc:
+      mfccs=np.mean(librosa.feature.mfcc(y=X,sr=sample_rate,n_mfcc=40).T,axis=0)
+      result=np.hstack((result,mfccs))
+    if chroma:
+      chromas=np.mean(librosa.feature.chroma_stft(S=stft,sr=sample_rate).T,axis=0)
+      result=np.hstack((result,chromas))
+    if mel:
+      mels=np.mean(librosa.feature.melspectrogram(X,sr=sample_rate).T,axis=0)
+      result=np.hstack((result,mels))
+    return result
+
+
+#DataFlair - Emotions in the RAVDESS dataset
+emotions={
+  '01':'neutral',
+  '02':'calm',
+  '03':'happy',
+  '04':'sad',
+  '05':'angry',
+  '06':'fearful',
+  '07':'disgust',
+  '08':'surprised'
+}
+#DataFlair - Emotions to observe
+observed_emotions=['calm', 'happy','angry', 'fearful', 'disgust']
+
+
+#DataFlair - Load the data and extract features for each sound file
+def load_data(test_size=0.25):
+    x,y=[],[]
+    for file in glob.glob("C:\\Users\\sachi\\www\\audio\\dataset\\Actor_*\\*.wav"):
+        file_name=os.path.basename(file)
+        emotion=emotions[file_name.split("-")[2]]
+        if emotion not in observed_emotions:
+            continue
+        feature=extract_feature(file, mfcc=True, chroma=True, mel=True)
+        x.append(feature)
+        y.append(emotion)
+    return train_test_split(np.array(x), y, test_size=test_size, random_state=9)
+x_train,x_test,y_train,y_test=load_data(test_size=0.25)
+model=MLPClassifier(alpha=0.01, batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500)
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+def load_new_data():
+    x,y=[],[]
+    for file in glob.glob("C:\\Users\\sachi\\www\\audio\\uploads\\*.wav"):
+        file_name=os.path.basename(file)
+        emotion=emotions[file_name.split("-")[2]]
+        if emotion not in observed_emotions:
+            continue
+        feature=extract_feature(file, mfcc=True, chroma=True, mel=True)
+        x.append(feature)
+        y.append(emotion)
+    return np.array(x),y
+xx_test,yy_test=load_new_data()
+yy_pred=model.predict(xx_test)
+print(yy_pred[len(yy_pred)-1]);
+accuracy=accuracy_score(y_true=y_test, y_pred=y_pred)
+print(" {:.2f}".format(accuracy*100))
---
+++ upload.php
@@ -0,0 +1,77 @@
+<?php
+$conn=mysqli_connect('localhost','root','','audiolibdb');
+if(!$conn)
+{
+  die('server not connected');
+}
+$query="select * from audios";
+$result=mysqli_query($conn,$query);
+$count=mysqli_num_rows($result)+1;
+$ans1="";
+$ans2="";
+if(isset($_POST['save_audio']) && $_POST['save_audio']=="Upload Audio")
+{
+
+  $dir='uploads/'.(string)$count;
+  $audio_path=$dir.basename($_FILES['audioFile']['name']);
+  if(move_uploaded_file($_FILES['audioFile']['tmp_name'],$audio_path)){
+    $command = escapeshellcmd('python test.py &');
+      $output = shell_exec($command);
+
+        $s="";
+        for($i=0;$i<strlen($output);$i++)
+        {
+          if($output[$i]==" ")
+          {
+            $ans1=$s;
+            $s="";
+          }
+          else {
+            $s=$s.$output[$i];
+          }
+        }
+        $ans2=$s;
+  }
+}
+function saveAudio($fileName){
+  $conn=mysqli_connect('localhost','root','','audiolibdb');
+  if(!$conn)
+  {
+    die('server not connected');
+  }
+  $query="insert into audios(filename) values ('{$fileName}')";
+  mysqli_query($conn,$query);
+  if(mysqli_affected_rows($conn)>0)
+  {
+    echo "audio file path save in database.";
+  }
+  mysqli_close($conn);
+}
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="text">
+      <h2>
+        <i class="fas fa-language"></i> Speech To Em<i class="far fa-laugh-beam bounce1"></i>ti<i class="fas fa-angry bounce2"></i>n Recognition
+      </h2>
+    </div>
+    <div class=" box1">
+      <div class="box2">
+        <div class="image">
+          <i class="fas fa-poll"></i>
+        </div>
+        <div class="new-file">
+        <div>The Person might be <?php  echo $ans1;?><div>
+        <div>The accuracy of the model is  <?php  echo $ans2;?> </div>
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
