688abe409384181ccfd19c85a85cd294efff6bf2
Sachin Gupta <58133378+sachin-14800@users.noreply.github.com>
2021-03-22 22:35:14 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -15,6 +15,7 @@ if(isset($_POST['save_audio']) && $_POST['save_audio']=="Upload Audio")
   $dir='uploads/'.(string)$count;
   $audio_path=$dir.basename($_FILES['audioFile']['name']);
   if(move_uploaded_file($_FILES['audioFile']['tmp_name'],$audio_path)){
+    saveAudio($audio_path);
     $command = escapeshellcmd('python test.py &');
       $output = shell_exec($command);
 
