cfaa883fc676f23dfb0eee75b932923a36d67a8f
Sachin Gupta <58133378+sachin-14800@users.noreply.github.com>
2021-03-27 13:35:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ $command = escapeshellcmd('python abc.py &');
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+   <link rel="icon" href="logo.jpg" type = "image/x-icon">
+        <title>Speech To Emotion Recognition</title>
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   </head>
